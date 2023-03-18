54e2eaf043c03df0cd1baef23a3301bfcdccc751
Juliana-Mr <juliana-marques-@hotmail.com>
2023-03-18 15:27:56 -0300

    typescript 1

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const database_1 = require("./database");
+console.log("Funcionou!!");
+console.log(database_1.user, database_1.product, database_1.purchase);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { user, product, purchase } from "./database";
+
+console.log("Funcionou!!")
+console.log(user, product, purchase)
